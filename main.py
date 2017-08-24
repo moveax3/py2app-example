2d0903f124d32e0032fd6b06689795ee9ac732c1
moveax3 <moveax3@gmail.com>
2017-08-24 11:03:36 +0500

    integrate chromedriver with app

--- main.py
+++
@@ -1,5 +0,0 @@
-import tkinter as tk
-root = tk.Tk()
-root.title("Sandwich")
-tk.Button(root, text="Hello world!").pack()
-tk.mainloop()
--- seleniumapp.py
+++ seleniumapp.py
@@ -1,16 +1,15 @@
-import tkinter as tk
-root = tk.Tk()
-root.title("Test py2app tkinter")
-tk.Button(root, text="Hello world!").pack()
-tk.mainloop()
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
+import os, sys, inspect
+
+current_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe() ))[0]))
+chromedriver = os.path.join(current_folder, "chromedriver")
+
+driver = webdriver.Chrome(executable_path=chromedriver)
 driver.get("http://google.ru")
 elem = driver.find_element_by_name("q")
 elem.clear()
-elem.send_keys("py2app with selenium working fine")
+elem.send_keys("py2app with custom locate chromedriver selenium working fine")
 elem.send_keys(Keys.RETURN)
-#driver.close()
+driver.close()
